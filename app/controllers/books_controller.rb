7b2674599d82fdea4dc108f2563bff69bf3aa3b7
yukiya <97089962+kuzirakotori@users.noreply.github.com>
2022-05-25 04:31:18 +0000

    [test crear]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,21 @@
 class BooksController < ApplicationController
   def new
-    @book=Book.new
   end
 
   def create
-    @book=Book.new(title: params[:title],body: params[:body])
+    @book=Book.new(title: params[:book][:title],body: params[:book][:body])
     if @book.save
       flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books=Book.all
-      render :new
+      render :index
     end
   end
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -28,19 +28,21 @@ class BooksController < ApplicationController
 
   def update
     book=Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice]="Book was successfully updated."
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book=Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end
   end
 
   def destroy
     book=Book.find(params[:id])
-    if book.destroy
-      flash[:notice]="Book was successfully destroyed."
-      redirect_to books_path
-    else
-      render :index
-    end
+    book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
